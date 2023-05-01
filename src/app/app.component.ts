d32c27593672fd983f56e5f0742a547f9af482f5
john clampet <jsclampet@gmail.com>
2023-04-30 20:44:28 -0500

    first commit, still need to figure out conditional styling to compare chars of userInput to chars of given text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'typing-challenge';
+  title: string = 'Typing Challenge';
+  text: string = 'Lorem ipsum dolor sit amet';
+  userInput: string = '';
+  index = this.userInput.length - 1;
+  textColor = '';
+
+  onUserInput (value: string) {
+    this.userInput = value; 
+    if(this.text[this.index] == this.userInput[this.index]){ 
+      this.textColor = 'green'
+    } else { 
+      this.textColor = "red"
+    }
+  }
+
 }
